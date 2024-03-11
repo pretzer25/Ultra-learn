065eb61f443dfda002a5abaf7a329cc2ce1afff0
n mnn <153140996+pretzer25@users.noreply.github.com>
2024-03-11 17:45:53 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://verce-vexg.vercel.app/bare',
+    bare: 'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
