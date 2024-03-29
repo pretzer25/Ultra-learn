bf458117b41ccbc3cb4a15d01ac5f9f0bfe77f44
Guy <153140996+pretzer25@users.noreply.github.com>
2024-03-29 17:15:30 -0500

    Update autoclick.js

--- scripts/autoclick.js
+++ scripts/autoclick.js
@@ -1,27 +1,33 @@
-var DELAY = 1;
-var autoClickerStyleElement = document.createElement("style");
-autoClickerStyleElement.innerHTML="*{cursor: crosshair !important;}";
-document.body.appendChild(autoClickerStyleElement);
-function addClicker(e) {
-	if(!e.isTrusted) {
-		return;
-	}
-	if(e.target.classList.contains("auto-clicker-target")) {
-		e.target.classList.remove("auto-clicker-target");
-	} else {
-		e.target.classList.add("auto-clicker-target");
-	}
-	document.body.removeChild(autoClickerStyleElement);
-	document.body.removeEventListener("click", addClicker);
-	e.preventDefault();
-	
-	autoClick(e.target);
+// auto-clicker.js
 
+var DELAY = 1000; // Delay in milliseconds (1 second)
+
+var autoClickerStyleElement = document.createElement("style");
+autoClickerStyleElement.innerHTML = "* { cursor: crosshair !important; }";
+document.body.appendChild(autoClickerStyleElement);
+
+function addClicker(e) {
+    if (!e.isTrusted) {
+        return;
+    }
+    if (e.target.classList.contains("auto-clicker-target")) {
+        e.target.classList.remove("auto-clicker-target");
+    } else {
+        e.target.classList.add("auto-clicker-target");
+    }
+    document.body.removeChild(autoClickerStyleElement);
+    document.body.removeEventListener("click", addClicker);
+    e.preventDefault();
+    autoClick(e.target);
 }
+
 function autoClick(element) {
-	if(element.classList.contains("auto-clicker-target")) {
-		element.click();
-		setTimeout(function(){ autoClick(element); }, DELAY);
-	}
+    if (element.classList.contains("auto-clicker-target")) {
+        element.click();
+        setTimeout(function () {
+            autoClick(element);
+        }, DELAY);
+    }
 }
-document.body.addEventListener("click", addClicker, 0);
+
+document.body.addEventListener("click", addClicker, false);
