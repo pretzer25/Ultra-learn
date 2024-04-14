e6eed51965126b30b107eb144104b6746d38aeb7
Guy <153140996+pretzer25@users.noreply.github.com>
2024-04-13 21:32:03 -0500

    Update escape.js

--- assets/js/escape.js
+++ assets/js/escape.js
@@ -1,6 +1,17 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+  const randomNumber = Math.floor(Math.random() * 24) + 1;
+
+  if (randomNumber === 1) {
 document.addEventListener("visibilitychange", function() {
   if (document.hidden) {
     const newWindow = window.open("about:blank", "_blank");
-    newWindow.document.write("<img src='https://thumbs.dreamstime.com/z/oh-no-2736328.jpg' style='width: 100vw; height: 100vh;'><script>alert('no escape');</script><script src='escape.js'></script>;");
+    newWindow.document.write("<img src='https://thumbs.dreamstime.com/z/oh-no-2736328.jpg' style='width: 100vw; height: 100vh;'><script>alert('no escape');</script><script src='escape1.js'></script>;");
 }
+});
+        console.log("No gar action this time.");
+  } else {
+    
+    console.log("No special action this time.");
+  }
 });
