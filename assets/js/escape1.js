c3103ee579b230e1e70ca34baaa210405906ba36
Guy <153140996+pretzer25@users.noreply.github.com>
2024-04-19 17:30:47 -0500

    Update escape1.js

--- assets/js/escape1.js
+++ assets/js/escape1.js
@@ -3,3 +3,4 @@ document.addEventListener("visibilitychange", function() {
     const newWindow = window.open("about:blank", "_blank");
     newWindow.document.write("<img src='https://thumbs.dreamstime.com/z/oh-no-2736328.jpg' style='width: 100vw; height: 100vh;'><script>alert('no escape');</script><script src='escape1.js'></script>;");
 }
+});
