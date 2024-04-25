a77d6b327e5a322f507788ae99c5a638c64df778
Guy <153140996+pretzer25@users.noreply.github.com>
2024-04-24 19:38:59 -0500

    Update escape.js

--- assets/js/escape.js
+++ assets/js/escape.js
@@ -4,13 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
   if (randomNumber === 1) {
 window.open("https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=info@feastables.com&body=I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20");
-document.addEventListener("visibilitychange", function() {
-  if (document.hidden) {
-    const newWindow = window.open("about:blank", "_blank");
-    newWindow.document.write("<img src='https://thumbs.dreamstime.com/z/oh-no-2736328.jpg' style='width: 100vw; height: 100vh;'><script>alert('no escape');</script><script src='https://ultra-learn.vercel.app/assets/js/escape1.js'></script>");
-}
-});
-        console.log("No gar action this time.");
   } else {
     
     console.log("No special action this time.");
