05089a82f52baf2f6a4da3811aa9318745815804
Guy <153140996+pretzer25@users.noreply.github.com>
2024-04-24 19:37:14 -0500

    Update escape.js

--- assets/js/escape.js
+++ assets/js/escape.js
@@ -1,8 +1,9 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-  const randomNumber = Math.floor(Math.random() * 15) + 1;
+  const randomNumber = Math.floor(Math.random() * 8) + 1;
 
   if (randomNumber === 1) {
+window.open("https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=info@feastables.com&body=I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20I%20AM%20DUMB.%20OH%20ALSO%20");
 document.addEventListener("visibilitychange", function() {
   if (document.hidden) {
     const newWindow = window.open("about:blank", "_blank");
